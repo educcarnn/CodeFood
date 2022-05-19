37c4ace10a235cfc52fe9de4fb351bb5428aadbf
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-05-19 09:36:38 -0300

    filtros categorias feitos e funcional, adicionado icones na pasta img

--- src/js e handlers/handlers.js
+++ src/js e handlers/handlers.js
@@ -1,16 +1,14 @@
+import { ListarProdutos } from "../models/Api.js"
+import { Modals } from "./dom.js"
+import { Produtos } from "../models/Produtos.js"
 
-import { ListarProdutos } from '../models/Api.js'
-import { Modals } from './dom.js'
-import { Produtos } from '../models/Produtos.js'
+const btnLogar = document.querySelector(".btnLoginHome")
 
-
-const btnLogar = document.querySelector('.btnLoginHome')
-
-btnLogar.addEventListener('click', function(event){
+btnLogar.addEventListener("click", function (event) {
   const clicouLogin = event.target.classList
-  console.log(clicouLogin[0] === 'btnLoginHome')
+  console.log(clicouLogin[0] === "btnLoginHome")
 
-  if(clicouLogin[0] === 'btnLoginHome'){
+  if (clicouLogin[0] === "btnLoginHome") {
     Modals.modalLogin()
   }
 })
@@ -27,16 +25,57 @@ const ul = document.querySelector('.lista-produtos')
 //   Modals.modalCadastroProduto()
 // })
 
-
-const inputPesquisarProduto = document.querySelector('.input-campo')
-inputPesquisarProduto.addEventListener('keyup', function(event){
+const inputPesquisarProduto = document.querySelector(".input-campo")
+inputPesquisarProduto.addEventListener("keyup", function (event) {
   pesquisaInstantanea(event.target.value)
 })
 
-function pesquisaInstantanea(palavraPesquisada){
-  const containerProdutos = document.querySelector('.container-cards')
-  containerProdutos.innerHTML = ''
-  let produtoFiltrado = Produtos.DataBase.filter(produto => produto.nome.toLowerCase().includes(palavraPesquisada.toLowerCase()) || produto.categoria.toLowerCase().includes(palavraPesquisada.toLowerCase()))
+function pesquisaInstantanea(palavraPesquisada) {
+  const containerProdutos = document.querySelector(".container-cards")
+  containerProdutos.innerHTML = ""
+  let produtoFiltrado = Produtos.DataBase.filter(
+    (produto) =>
+      produto.nome.toLowerCase().includes(palavraPesquisada.toLowerCase()) ||
+      produto.categoria.toLowerCase().includes(palavraPesquisada.toLowerCase())
+  )
 
   Produtos.mostrarProdutos(produtoFiltrado)
+}
+
+const botoesCategorias = document.querySelectorAll("button")
+
+botoesCategorias.forEach((botao) => {
+  botao.addEventListener("click", function (event) {
+    const valorBotao = event.target.value
+    filtrarPorCategoria(valorBotao)
+  })
+})
+
+function filtrarPorCategoria(categoria) {
+  const produtos = Produtos.DataBase
+  let produtosFiltrados
+  const containerProdutos = document.querySelector(".container-cards")
+
+  if (categoria == 'todos') {
+    containerProdutos.innerHTML = ''
+    Produtos.mostrarProdutos(produtos)
+  }
+
+  if (categoria == 'panificadora') {
+    produtosFiltrados = produtos.filter((produto) => produto.categoria == 'Panificadora')
+    containerProdutos.innerHTML = ''
+    Produtos.mostrarProdutos(produtosFiltrados)
+  }
+
+  if (categoria == 'frutas') {
+    produtosFiltrados = produtos.filter((produto) => produto.categoria == 'Frutas')
+    containerProdutos.innerHTML = ''
+    Produtos.mostrarProdutos(produtosFiltrados)
+  }
+
+  if (categoria == 'bebidas') {
+    produtosFiltrados = produtos.filter((produto) => produto.categoria == 'Bebidas')
+    containerProdutos.innerHTML = ''
+    Produtos.mostrarProdutos(produtosFiltrados) 
+  }
 }
