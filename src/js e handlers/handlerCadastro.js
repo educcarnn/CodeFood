0a90df4689e956812cd9187a4a2b6a7830728ca1
arc4dex <pedroiago.bernardes@gmail.com>
2022-05-19 20:39:08 -0300

    função de adicionar produto funcionando

--- src/js e handlers/handlerCadastro.js
+++ src/js e handlers/handlerCadastro.js
@@ -1,9 +1,16 @@
 import { Modals } from "./dom.js";
 
-const btLogin = document.querySelector('.homePage')
+const btLogin = document.querySelector('.btnCadastrar')
+const btnHome = document.querySelector('.homePage')
 
-btLogin.addEventListener('click', function(e){
-  e.target.preventDefault()
-  console.log(e)
+btLogin.addEventListener('click', function(event){
+  const clicouCadastrar = event.target.classList
+  console.log(event)
 })
+
+btnHome.addEventListener('click', function(){
+  window.location = '/index.html'
+})
+
+
 
